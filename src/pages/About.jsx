79c87fb75a4d053b23704c80dbf98f97ad96ea8a
KyleGru <136273960+KyleGru@users.noreply.github.com>
2024-03-20 13:19:39 -0400

    Updated filler about me section with actual about me information

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,12 +12,13 @@ export default function About() {
                     <h4 className="title">Junior Full Stack Web Developer</h4>
                     <section className="right-container text-left">
                         <p>
-                            Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-                            Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-                            mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-                            lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-                            imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-                            in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+                        Hey there! I'm Kyle Gruschow, a passionate learner and aspiring full stack web developer currently enrolled in an online Full Stack Web Development Bootcamp through UCLA. My journey into the world of coding began with a curiosity to understand how websites are built and a desire to create digital experiences that make a difference.<br></br>
+
+                        <br></br>Throughout the bootcamp, I've had the opportunity to dive deep into various coding technologies, mastering languages like HTML, CSS, JavaScript, and frameworks like React.js and Node.js. I've also gained valuable experience working with databases such as MySQL and MongoDB, enabling me to build dynamic and interactive web applications.<br></br>
+
+                        <br></br>What drives me in my journey as a web developer is the constant inspiration to keep learning and exploring new languages and technologies. The ever-evolving nature of the tech industry fuels my passion to stay updated with the latest trends and advancements. I believe that the key to success in this field lies in continuous learning and adapting to new challenges.<br></br>
+
+                        <br></br>Beyond coding, I'm fueled by creativity and problem-solving. I enjoy collaborating with others to turn ideas into reality and strive to create meaningful and impactful solutions through my work. Join me on this exciting journey as I continue to expand my skills, tackle new challenges, and build innovative web experiences that make a difference.
                         </p>
                     </section>
                 </Col>
